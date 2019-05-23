271bcbd8cab69c887d6d96eb19e48546fff28b2f
Marcus Bass <Bass.Marcus@icloud.com>
2019-05-22 21:54:23 -0700

    background set and divs aligned. non-responsive

---
+++ app/src/components/Splash/Splash.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Splash.css'
+
+export default class Splash extends Component {
+    render() {
+        return (
+            <div className="SplashComponent">
+                <div className="firstColumn">
+
+                </div>
+
+                <div className="secondColumn">
+
+                </div>
+
+                <div className="thirdColumn">
+
+                </div>
+            </div>
+        )
+    }
+}

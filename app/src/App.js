4645ca1874f91749d336cfe000b535acf36e5d7e
Marcus Bass <Bass.Marcus@icloud.com>
2019-06-10 18:14:20 -0700

    init

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         This is an init commit.
         </a>
       </header>
     </div>

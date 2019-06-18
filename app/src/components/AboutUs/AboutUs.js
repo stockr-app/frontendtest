198243668277861f2305c235394f366fd52ce9df
Mitchell Robles <mitchellr92@live.com>
2019-06-18 12:51:18 -0700

    cleaned up styling

--- app/src/components/AboutUs/AboutUs.js
+++ app/src/components/AboutUs/AboutUs.js
@@ -16,51 +16,51 @@ class AboutUs extends Component {
         <div className="image-container">
           <div className="profile-image">
             <img src={AA} alt="AA" className="image" />
-            <div>Andrew Armstrong</div>
+            <div className="name">Andrew Armstrong</div>
             <a className="link" href="https://github.com/acarmstrong98">
-              <i class="fab fa-github" />
+              <i className="fab fa-github" />
             </a>
             <a className="link" href="">
-              <i class="fab fa-linkedin" />
+              <i className="fab fa-linkedin" />
             </a>
           </div>
           <div className="profile-image">
             <img src={AO} alt="AO" className="image" />
-            <div>Andrew Ogle</div>
+            <div className="name">Andrew Ogle</div>
             <a className="link" href="https://github.com/andrewogle">
-              <i class="fab fa-github" />
+              <i className="fab fa-github" />
             </a>
             <a
               className="link"
               href="https://www.linkedin.com/in/andrew-ogle-574ba759/"
             >
-              <i class="fab fa-linkedin" />
+              <i className="fab fa-linkedin" />
             </a>
           </div>
           <div className="profile-image">
             <img src={MB} alt="MB" className="image" />
-            <div>Marcus Bass</div>
+            <div className="name">Marcus Bass</div>
             <a className="link" href="https://github.com/marcusbass323">
-              <i class="fab fa-github" />
+              <i className="fab fa-github" />
             </a>
             <a
               className="link"
               href="https://www.linkedin.com/in/marcus-bass-7598b6106/"
             >
-              <i class="fab fa-linkedin" />
+              <i className="fab fa-linkedin" />
             </a>
           </div>
           <div className="profile-image">
             <img src={MR} alt="MR" className="image" />
-            <div>Mitchell Robles</div>
+            <div className="name">Mitchell Robles</div>
             <a className="link" href="https://github.com/mitchellr92">
-              <i class="fab fa-github" />
+              <i className="fab fa-github" />
             </a>
             <a
               className="link"
               href="https://www.linkedin.com/in/mitchell-robles-aa7980165/"
             >
-              <i class="fab fa-linkedin" />
+              <i className="fab fa-linkedin" />
             </a>
           </div>
         </div>

7758e4a5b8788fa8fe8697a746a047716708d7bb
Mitchell Robles <mitchellr92@live.com>
2019-06-18 10:52:46 -0700

    Added about us component and link to nav bar

---
+++ app/src/components/AboutUs/AboutUs.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+class AboutUs extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div>About Us</div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default AboutUs;

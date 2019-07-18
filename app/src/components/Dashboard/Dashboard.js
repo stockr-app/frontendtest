babd6c7380ad4d146f0149d76c2cb10cbe75704e
Andrew <andrewryanogle@gmail.com>
2019-07-17 19:03:51 -0700

    re push

--- app/src/components/Dashboard/Dashboard.js
+++ app/src/components/Dashboard/Dashboard.js
@@ -36,6 +36,7 @@ export default class Dashboard extends Component {
                     <Newsview />
                     </div>
                 </div>
+                </div>
         )
     }
 }

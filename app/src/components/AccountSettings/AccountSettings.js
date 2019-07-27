df6afbcdd1f2d1fecb2443276b2941a12d138e78
Marcus Bass <Bass.Marcus@icloud.com>
2019-07-26 21:07:15 -0700

    settings drawer done

--- app/src/components/AccountSettings/AccountSettings.js
+++ app/src/components/AccountSettings/AccountSettings.js
@@ -6,7 +6,9 @@ export default class AccountSettings extends Component {
         return (
             <div>
                 <h1>Account Settings</h1>
-                
+                <p>Education Mode</p>
+                <p>Dark/Light Mode</p>
+                <h1>Billing</h1>
             </div>
         )
     }
--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import moment from 'moment';
 import "./Header.css";
 import Countdown from '../Countdown/Countdown';
+import AccountSettings from '../AccountSettings/AccountSettings';
+import Billing from '../Billing/Billing';
 export default class Header extends Component {
       
       state = {
@@ -24,6 +26,14 @@ export default class Header extends Component {
         document.getElementById("myNav").style.width = "0%";
       }
 
+      openAccountSettings(){
+        document.getElementById("AccountSettings").style.width = "25%";
+      }
+  
+      closeAccountSettings(){
+        document.getElementById("AccountSettings").style.width = "0%";
+      }
+
       Logout(){
         sessionStorage.removeItem("STATE");
       }
@@ -38,8 +48,7 @@ export default class Header extends Component {
             <p className="todaysDate">{moment().format("MMMM-DD-YYYY")}</p>
             <div className="icons" >
                 <a href="#/"><i className="fas fa-inbox"></i></a>
-                <a href="#/"><i className="fas fa-cog" /></a>
-                <a href="#/"><i className="fas fa-file-invoice-dollar" /></a>
+                <a href="#/" onClick={this.openAccountSettings}><i className="fas fa-cog" /></a>
                 <a href="#/" onClick={this.openScreenSaver}><i className="fas fa-cloud-moon"></i></a>
               </div>
           </div>
@@ -49,11 +58,17 @@ export default class Header extends Component {
             </div>
         </div>
                <div id="myNav" className="overlay">
-             <a href="#/" className="closebtn" onClick={this.closeScreenSaver}>&times;</a>
+                  <a href="#/" className="closebtn" onClick={this.closeScreenSaver}>&times;</a>
                <div className="overlay-content">
-               <Countdown />
+                <Countdown />
                </div>
-           </div>
+              </div>
+              <div id="AccountSettings" className="AccountSettings">
+                  <a href="#/" className="closebtn" onClick={this.closeAccountSettings}>&times;</a>
+               <div className="overlay-content">
+                <AccountSettings />
+               </div>
+              </div>
       </div>
     ) 
   }
